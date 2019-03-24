ff4a5e5dce165eb901d2648651749607bbfdf913
Innokentiy Mokin <enjection@ya.ru>
2019-03-24 13:49:19 +0700

    remove unnecessary comments

--- rights_manager.h
+++ rights_manager.h
@@ -8,22 +8,15 @@
 class rights_manager
 {
     std::map<std::string, bool> resources_;
-    //user -> resource -> bool
     std::map<std::string, std::map<std::string, bool>> user_rights_;
 public:
-    // "projects.<project_name>.read"
-    // "projects.<project_name>.write"
-    // "projects.<project_name>.exec"
-    // "admin"
     void add_resource(
             const std::string& resource_name,
             bool default_value = false);
-    // "enjection" "projects.internal.read" true
     void set_right(
             const std::string& username,
             const std::string& resource_name,
             bool value);
-    // "enjection" "projects.internal.read" -> true
     std::optional<bool> check_right(
             const std::string& username,
             const std::string& resource_name);
