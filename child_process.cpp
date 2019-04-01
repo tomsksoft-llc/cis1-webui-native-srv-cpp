d857bf86852ed12ebb6a9190dd3388671e6d1a93
Innokentiy Mokin <enjection@ya.ru>
2019-04-01 17:13:02 +0700

    update child_process todo

--- child_process.cpp
+++ child_process.cpp
@@ -31,13 +31,13 @@ void child_process::run(
         bp::search_path(programm),
         env_,
         ctx_,
-        //strand_.context(), //TODO
         bp::std_out > boost::asio::buffer(buffer_),
         bp::start_dir = start_dir_.c_str(),
         bp::args = args,
         bp::on_exit = 
         [&, cb, self = shared_from_this()](int exit, const std::error_code& ec)
         {
+            //TODO run in strand_.context();
             cb(exit, buffer_, ec);
         });
 }
