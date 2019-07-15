85f9f463db493a9acb70b714911dfecd619189d3
Innokentiy Mokin <enjection@ya.ru>
2019-07-15 17:58:33 +0700

    add meta_converter

---
+++ tpl_reflect/json_engine.h
@@ -0,0 +1,370 @@
+#pragma once
+
+#include <rapidjson/document.h>
+
+#include "meta_converter.h"
+
+namespace json
+{
+
+struct engine
+{
+    using obj_type = rapidjson::Value;
+
+    using set_context = rapidjson::Document::AllocatorType&;
+
+    static bool has(
+            const obj_type& protocol_obj,
+            const char* str);
+
+    template <class FieldType>
+    static bool get(
+            const obj_type& protocol_obj,
+            const char* str,
+            FieldType& obj_field);
+
+    template <class FieldType>
+    static bool get(
+            const obj_type& protocol_obj,
+            const char* str,
+            std::vector<FieldType>& obj_field);
+
+    template <class FieldType>
+    static bool get(
+            const obj_type& protocol_obj,
+            const char* str,
+            std::map<std::string, FieldType>& obj_field);
+
+    static bool get(
+            const obj_type& protocol_obj,
+            const char* str);
+
+    template <class FieldType>
+    static void set(
+            obj_type& protocol_obj,
+            const char* str,
+            const FieldType& obj_field,
+            set_context ctx);
+
+    template <class FieldType>
+    static void set(
+            obj_type& protocol_obj,
+            const char* str,
+            const std::vector<FieldType>& obj_field,
+            set_context ctx);
+
+    template <class FieldType>
+    static void set(
+            obj_type& protocol_obj,
+            const char* str,
+            const std::map<std::string, FieldType>& obj_field,
+            set_context ctx);
+
+    static void set(
+            obj_type& protocol_obj,
+            const char* str,
+            set_context ctx);
+};
+
+template <class T>
+bool get(const rapidjson::Value& from, T& to)
+{
+	auto conv = to.get_converter();
+	return conv.template get<engine>(from, to);
+}
+
+template <class T>
+void set(
+        rapidjson::Value& to,
+        const T& from,
+        engine::set_context ctx)
+{
+	to.SetObject();
+	from.get_converter().template set<engine>(to, from, ctx);
+}
+
+template <class T>
+void set(
+        rapidjson::Value& to,
+        const char* field_name,
+        const T& from,
+        engine::set_context ctx)
+{
+	rapidjson::Value v;
+    set(v, from, ctx);
+    to.AddMember(
+            rapidjson::Value().SetString(
+                    field_name,
+                    strlen(field_name),
+                    ctx),
+            v.Move(),
+            ctx);
+}
+
+bool engine::has(
+        const obj_type& protocol_obj,
+        const char* str)
+{
+    return protocol_obj.HasMember(str);
+}
+
+template <class FieldType>
+bool engine::get(
+        const obj_type& protocol_obj,
+        const char* str,
+        FieldType& obj_field)
+{
+    auto& field = protocol_obj[str];
+    return json::get(field, obj_field);
+}
+
+template <class FieldType>
+bool engine::get(
+        const obj_type& protocol_obj,
+        const char* str,
+        std::vector<FieldType>& obj_field)
+{
+    if(protocol_obj[str].IsArray())
+    {
+        for(const auto& param : protocol_obj[str].GetArray())
+        {
+            FieldType arr_obj;
+
+            if constexpr (reflect::has_converter<FieldType>)
+            {
+                if(param.IsObject())
+                {
+                    FieldType::get_converter().template get<engine>(param, arr_obj);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                json::get(param, arr_obj);
+            }
+
+            obj_field.push_back(arr_obj);
+        }
+        return true;
+    }
+
+    return false;
+}
+
+template <class FieldType>
+bool engine::get(
+        const obj_type& protocol_obj,
+        const char* str,
+        std::map<std::string, FieldType>& obj_field)
+{
+    if(protocol_obj[str].IsObject())
+    {
+        for(const auto& [key, value] : protocol_obj[str].GetObject())
+        {
+            if(key.IsString())
+            {
+                FieldType map_obj;
+                if constexpr (reflect::has_converter<FieldType>)
+                {
+                    if(value.IsObject())
+                    {
+                        FieldType::get_converter().template get<engine>(value, map_obj);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    json::get(value, map_obj);
+                }
+                obj_field.insert({key.GetString(), map_obj});
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    return false;
+}
+
+bool engine::get(
+        const obj_type& protocol_obj,
+        const char* str)
+{
+    return protocol_obj[str].IsObject() ? true : false;
+}
+
+template <class FieldType>
+void engine::set(
+        obj_type& protocol_obj,
+        const char* str,
+        const FieldType& obj_field,
+        set_context ctx)
+{
+    return json::set(protocol_obj, str, obj_field, ctx);
+}
+
+template <class FieldType>
+void engine::set(
+        obj_type& protocol_obj,
+        const char* str,
+        const std::vector<FieldType>& obj_field,
+        set_context ctx)
+{
+    rapidjson::Value v;
+    v.SetArray();
+    for(const auto& obj : obj_field)
+    {
+        rapidjson::Value vv;
+        if constexpr (reflect::has_converter<FieldType>)
+        {
+            vv.SetObject();
+            FieldType::get_converter().template set<engine>(vv, obj, ctx);
+        }
+        else
+        {
+            json::set(vv, obj, ctx);
+        }
+        v.PushBack(vv.Move(), ctx);
+    }
+    protocol_obj.AddMember(
+            rapidjson::Value().SetString(
+                    str,
+                    strlen(str),
+                    ctx),
+            v.Move(),
+            ctx);
+}
+
+template <class FieldType>
+void engine::set(
+        obj_type& protocol_obj,
+        const char* str,
+        const std::map<std::string, FieldType>& obj_field,
+        set_context ctx)
+{
+    rapidjson::Value v;
+    v.SetObject();
+    for(const auto& [key, value] : obj_field)
+    {
+        rapidjson::Value vv;
+        if constexpr (reflect::has_converter<FieldType>)
+        {
+            vv.SetObject();
+            FieldType::get_converter().template set<engine>(vv, value, ctx);
+        }
+        else
+        {
+            json::set(vv,value, ctx);
+        }
+        v.AddMember(
+                rapidjson::Value().SetString(
+                        key.c_str(),
+                        key.length(),
+                        ctx),
+                vv.Move(),
+                ctx);
+    }
+    protocol_obj.AddMember(
+            rapidjson::Value().SetString(
+                    str,
+                    strlen(str),
+                    ctx),
+            v.Move(),
+            ctx);
+}
+
+void engine::set(
+        obj_type& protocol_obj,
+        const char* str,
+        set_context ctx)
+{
+    protocol_obj.AddMember(
+            rapidjson::Value().SetString(
+                    str,
+                    strlen(str),
+                    ctx),
+            rapidjson::Value().SetObject(),
+            ctx);
+}
+
+} // namespace json
+
+namespace json
+{
+
+template <>
+bool get<int32_t>(const rapidjson::Value& from, int32_t& to)
+{
+    if(from.IsInt())
+    {
+        to = from.GetInt();
+        return true;
+    }
+
+    return false;
+}
+
+template <>
+bool get<uint64_t>(const rapidjson::Value& from, uint64_t& to)
+{
+    if(from.IsUint64())
+    {
+        to = from.GetUint64();
+        return true;
+    }
+
+    return false;
+}
+
+template <>
+bool get<std::string>(const rapidjson::Value& from, std::string& to)
+{
+    if(from.IsString())
+    {
+        to = from.GetString();
+        return true;
+    }
+
+    return false;
+}
+
+template <>
+void set<int32_t>(
+        rapidjson::Value& to,
+        const int32_t& from,
+        engine::set_context allocator)
+{
+    to.SetInt(from);
+}
+
+template <>
+void set<uint64_t>(
+        rapidjson::Value& to,
+        const uint64_t& from,
+        engine::set_context allocator)
+{
+    to.SetUint64(from);
+}
+
+template <>
+void set<std::string>(
+        rapidjson::Value& to,
+        const std::string& from,
+        engine::set_context allocator)
+{
+    to.SetString(
+            from.c_str(),
+            from.length(),
+            allocator);
+}
+
+} // namespace json
---
+++ tpl_reflect/meta_converter.h
@@ -0,0 +1,320 @@
+#pragma once
+
+#include "tpl_helpers/ct_string.h"
+#include "tpl_helpers/detect_idiom.h"
+
+namespace reflect
+{
+
+template <class... T>
+class fields_pack{};
+
+template <class... T>
+class validators_pack{};
+
+template <class... Args>
+struct meta_converter;
+
+template <class Type, class CTString>
+struct ignored_field
+{
+    constexpr ignored_field(std::tuple<>)
+    {}
+
+    template <class ProtocolEngine>
+    static bool has(
+            const typename ProtocolEngine::obj_type& protocol_obj)
+    {
+        return ProtocolEngine::has(
+                protocol_obj,
+                CTString::value);
+    }
+
+    template <class ProtocolEngine>
+    static bool get(
+            const typename ProtocolEngine::obj_type& protocol_obj,
+            Type& obj)
+    {
+        return ProtocolEngine::get(
+                protocol_obj,
+                CTString::value);
+    }
+
+    template <class ProtocolEngine>
+    static bool set(
+            typename ProtocolEngine::obj_type& protocol_obj,
+            const Type& obj,
+            typename ProtocolEngine::set_context ctx)
+    {
+        ProtocolEngine::set(
+                protocol_obj,
+                CTString::value,
+                ctx);
+        return true; //just for paramater pack unfolding simplification
+    }
+
+    bool validate(
+            const Type& obj) const
+    {
+        return true;
+    }
+};
+
+template <
+        class Type,
+        class FieldType,
+        FieldType Type::* Ptr,
+        class CTString,
+        class... Validators>
+struct field
+    : private Validators...
+{
+    constexpr field(field&& f)
+        : Validators(std::move(static_cast<Validators&>(f)))...
+    {}
+
+	template <class Tuple>
+    constexpr field(Tuple tup)
+        : field(tup, std::make_index_sequence<std::tuple_size<Tuple>::value>{})
+    {}
+
+    template <class Tuple, size_t... Is>
+    constexpr field(Tuple tup, std::index_sequence<Is...>)
+        : Validators(std::get<Is>(tup))...
+    {}
+
+    template <class ProtocolEngine>
+    static bool has(
+            const typename ProtocolEngine::obj_type& protocol_obj)
+    {
+        return ProtocolEngine::has(
+                protocol_obj,
+                CTString::value);
+    }
+
+    template <class ProtocolEngine>
+    static bool get(
+            const typename ProtocolEngine::obj_type& protocol_obj,
+            Type& obj)
+    {
+        auto& obj_field = obj.*Ptr;
+        return ProtocolEngine::get(
+                protocol_obj,
+                CTString::value,
+                obj_field);
+    }
+
+    template <class ProtocolEngine>
+    static bool set(
+            typename ProtocolEngine::obj_type& protocol_obj,
+            const Type& obj,
+            typename ProtocolEngine::set_context ctx)
+    {
+        const auto& obj_field = obj.*Ptr;
+        ProtocolEngine::set(
+                protocol_obj,
+                CTString::value,
+                obj_field,
+                ctx);
+        return true; //just for paramater pack unfolding simplification
+    }
+
+    bool validate(
+            const Type& obj) const
+    {
+        return ((static_cast<const Validators*>(this)->operator()(obj.*Ptr)) && ...);
+    }
+};
+
+template <
+        class Type,
+        class FnType,
+        auto... Ptrs>
+struct validator
+    : private FnType
+{
+    constexpr validator(validator&& v)
+        : FnType(std::move(static_cast<FnType&>(v)))
+    {}
+
+    constexpr validator(FnType fn)
+        : FnType(fn)
+    {}
+
+    bool validate(
+            const Type& obj) const
+    {
+        return FnType::operator()(obj.*Ptrs...);
+    }
+};
+
+template <class Type, class... Fields, class... Validators>
+struct meta_converter<
+        Type,
+        fields_pack<Fields...>,
+        validators_pack<Validators...>>
+        : private Fields...
+        , private Validators...
+{
+    constexpr meta_converter(
+            Fields&&... field_args,
+            Validators&&... validator_args)
+		: Fields(std::move(field_args))...
+        , Validators(std::move(validator_args))...
+    {}
+
+    template <
+			class FieldsArgsTuple,
+			class... ValidatorArgs>
+    constexpr meta_converter(
+            FieldsArgsTuple fields_args,
+            ValidatorArgs... validator_args)
+		: meta_converter(
+				fields_args,
+				std::make_index_sequence<std::tuple_size<FieldsArgsTuple>::value>{},
+				validator_args...)
+    {}
+
+    template <
+			class FieldsArgsTuple,
+			size_t... Is,
+			class... ValidatorArgs>
+    constexpr meta_converter(
+            FieldsArgsTuple fields_args,
+            std::index_sequence<Is...>,
+			ValidatorArgs... validator_args)
+		: Fields(std::get<Is>(fields_args))...
+        , Validators(validator_args)...
+    {}
+
+    template <class ProtocolEngine>
+    bool has(
+            const typename ProtocolEngine::obj_type& protocol_obj) const
+    {
+        return (Fields::template has<ProtocolEngine>(protocol_obj) && ...);
+    }
+
+    template <class ProtocolEngine>
+    bool get(
+            const typename ProtocolEngine::obj_type& protocol_obj,
+            Type& obj) const
+    {
+        return (Fields::template get<ProtocolEngine>(protocol_obj, obj) && ...);
+    }
+
+    template <class ProtocolEngine>
+    bool set(
+            typename ProtocolEngine::obj_type& protocol_obj,
+            const Type& obj,
+            typename ProtocolEngine::set_context ctx) const
+    {
+        return (Fields::template set<ProtocolEngine>(protocol_obj, obj, ctx) && ...);
+    }
+
+    bool validate(const Type& obj) const
+    {
+        return ((static_cast<const Fields*>(this)->validate(obj)) && ...)
+            && ((static_cast<const Validators*>(this)->validate(obj)) && ...);
+    }
+};
+
+template<typename T>
+using converter_t = decltype( T::get_converter() );
+
+template<typename T>
+constexpr bool has_converter = meta::is_detected<converter_t, T>::value;
+
+namespace detail
+{
+
+template <auto c>
+struct ptr_v{};
+
+template <class... Args>
+struct meta_converter_impl;
+
+template <class Type, class... Fields, class... Validators>
+struct meta_converter_impl<
+        Type,
+        fields_pack<Fields...>,
+        validators_pack<Validators...>>
+        : public Fields...
+        , public Validators...
+{
+    constexpr meta_converter_impl(){}
+
+    template <class... PrevFields, class... PrevValidators, class Entry>
+    constexpr meta_converter_impl(
+            meta_converter_impl<
+                    Type,
+                    fields_pack<PrevFields...>,
+                    validators_pack<PrevValidators...>>&& prev,
+            Entry&& entry)
+        : PrevFields(std::move(static_cast<PrevFields&>(prev)))...
+        , PrevValidators(std::move(static_cast<PrevValidators&>(prev)))...
+        , Entry(std::move(entry))
+    {}
+
+    template <
+            class CTString,
+            class FieldType,
+            FieldType Type::* Ptr,
+            class... FieldValidators>
+    constexpr meta_converter_impl<
+            Type,
+            fields_pack<Fields...,
+                    field<Type, FieldType, Ptr, CTString, FieldValidators...>>,
+            validators_pack<Validators...>> add_field(
+            CTString ct_string,
+            ptr_v<Ptr> ptr,
+            FieldValidators... field_validators)
+    {
+        field<Type, FieldType, Ptr, CTString, FieldValidators...> f(
+                std::make_tuple(field_validators...));
+        return {std::move(*this), std::move(f)};
+    }
+
+    template <
+            class Validator,
+            auto Type::*... Ptrs>
+    constexpr meta_converter_impl<
+            Type,
+            fields_pack<Fields...>,
+            validators_pack<Validators...,
+                    validator<Type, Validator, Ptrs...>
+                    >> add_validator(
+            Validator fn,
+            ptr_v<Ptrs>... ptrs)
+    {
+        /*
+        static_assert(
+                is_applicable<Validator, Args2...>::value,
+                "meta_converter_impl::add_validator():"
+                " argument types doesn't match field types");
+        */
+        validator<Type, Validator, Ptrs...> v(fn);
+        return {std::move(*this), std::move(v)};
+    }
+
+    constexpr meta_converter<
+            Type,
+            fields_pack<Fields...>,
+            validators_pack<Validators...>> done()
+    {
+        return {std::move(static_cast<Fields&>(*this))...,
+                std::move(static_cast<Validators&>(*this))...};
+    }
+};
+
+} // namespace detail
+
+template <auto c>
+using ptr_v = detail::ptr_v<c>;
+
+template <class T>
+constexpr auto make_meta_converter()
+{
+    return detail::meta_converter_impl<T, fields_pack<>, validators_pack<>>{};
+}
+
+} // namespace reflect
