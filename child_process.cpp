4e5b3c36811a2f92d9afe704fd7fd247dc6dfdfa
Innokentiy Mokin <enjection@ya.ru>
2019-07-28 14:56:32 +0700

    add stdout support to child_process

--- child_process.cpp
+++ child_process.cpp
@@ -10,17 +10,9 @@ child_process::child_process(
     : ctx_(ctx)
     , env_(env)
     , start_dir_(std::filesystem::path{cis::get_root_dir()} / cis::core)
-{
-    //buffer_.resize(1024);
-}
-
-child_process::~child_process()
-{
-    if(proc_ != nullptr)
-    {
-        delete proc_;
-    }
-}
+    , pipe_(ctx)
+    , proc_(nullptr)
+{}
 
 void child_process::set_interactive_params(
         const std::vector<std::string>& params)
@@ -31,12 +23,10 @@ void child_process::set_interactive_params(
 void child_process::run(
         const std::string& programm,
         std::vector<std::string> args,
-        const std::function<void(
-                int,
-                std::vector<char>&,
-                const std::error_code&)>& cb)
+        const on_exit_cb_t& cb)
 {
     namespace bp = boost::process;
+
     for(auto& param : interactive_params_)
     {
         std::copy(
@@ -45,27 +35,64 @@ void child_process::run(
                 std::back_inserter(buffer_));
         buffer_.push_back('\n');
     }
+
+    cb_ = cb;
+
     try
     {
-        proc_ = new bp::child(
+        proc_ = std::make_unique<bp::child>(
                 programm,
                 env_,
                 ctx_,
                 bp::std_in < boost::asio::buffer(buffer_),
-                //bp::std_out > boost::asio::buffer(buffer_),
+                bp::std_out > pipe_,
                 bp::start_dir = start_dir_.c_str(),
                 bp::args = args,
                 bp::on_exit =
-                        [&, cb, self = shared_from_this()](
-                                int exit,
+                        [&, self = shared_from_this()](
+                                int exit_code,
                                 const std::error_code& ec)
                         {
-                            //TODO run in strand_.context();
-                            //cb(exit, buffer_, ec);
+                            exit_code_ = exit_code;
+                            do_read(ec, std::move(self));
                         });
     }
     catch(const boost::process::process_error& e)
     {
         std::cerr << e.what() << std::endl;
+    }
+}
+
+void child_process::do_read(
+        const std::error_code& ec,
+        std::shared_ptr<child_process> self,
+        size_t offset)
+{
+    //TODO handle errors
+    buffer_.resize(offset + read_size);
+    boost::asio::async_read(
+            pipe_,
+            boost::asio::buffer(buffer_.data(), buffer_.size()),
+            [&, self = std::move(self)](
+                const std::error_code& ec,
+                size_t transferred)
+            {
+                if(transferred < read_size)
+                {
+                    buffer_.resize(buffer_.size() - (read_size - transferred));
+                    on_done();
+                }
+                else
+                {
+                    do_read(ec, std::move(self), buffer_.size());
+                }
+            });
+}
+
+void child_process::on_done()
+{
+    if(cb_)
+    {
+        cb_(exit_code_.value(), buffer_);
     }
 }
--- child_process.h
+++ child_process.h
@@ -2,19 +2,21 @@
 
 #include <filesystem>
 #include <vector>
+#include <memory>
 
 #include <boost/process.hpp>
 
-//TODO: write better abstraction for better interacting
 class child_process
     : public std::enable_shared_from_this<child_process>
 {
 public:
+    using on_exit_cb_t = std::function<void(
+                    int,
+                    const std::vector<char>&)>;
+
     child_process(
             boost::asio::io_context& ctx,
             const boost::process::environment& env);
-
-    ~child_process();
 
     void set_interactive_params(
             const std::vector<std::string>& params);
@@ -22,15 +24,24 @@ public:
     void run(
             const std::string& programm,
             std::vector<std::string> args,
-            const std::function<void(
-                    int,
-                    std::vector<char>&,
-                    const std::error_code&)>& cb);
+            const on_exit_cb_t& cb);
+
 private:
     boost::asio::io_context& ctx_;
     boost::process::environment env_;
     std::filesystem::path start_dir_;
+    boost::process::async_pipe pipe_;
     std::vector<char> buffer_;
     std::vector<std::string> interactive_params_;
-    boost::process::child *proc_ = nullptr;
+    std::unique_ptr<boost::process::child> proc_;
+    on_exit_cb_t cb_;
+    const size_t read_size = 512;
+    std::optional<int> exit_code_;
+
+    void do_read(
+            const std::error_code& ec,
+            std::shared_ptr<child_process> self,
+            size_t offset = 0);
+
+    void on_done();
 };
