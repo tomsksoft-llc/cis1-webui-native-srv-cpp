c836db312325bd04c5cef3e29689d9dd18a5725a
Innokentiy Mokin <enjection@ya.ru>
2019-09-23 08:44:35 +0700

    add test_package

---
+++ test_package/conanfile.py
@@ -0,0 +1,20 @@
+from conans.model.conan_file import ConanFile
+from conans import CMake
+
+
+class Cis1CoreNativeTests(ConanFile):
+    settings = "os", "compiler", "arch", "build_type"
+    generators = "cmake"
+    requires = "gtest/1.8.1@bincrafters/stable"
+
+    def build(self):
+        self.cmake = CMake(self)
+        self.cmake.configure()
+        self.cmake.build()
+
+    def test(self):
+        target_test = "RUN_TESTS" if self.settings.os == "Windows" else "test"
+        self.cmake.build(target=target_test)
+
+    def imports(self):
+        self.copy("FindFilesystem.cmake", dst="cmake/modules", src="cmake/modules")
