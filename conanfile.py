d33bbcb680831714b0213af2bb5f99c19795bbeb
Innokentiy Mokin <enjection@ya.ru>
2019-10-24 12:30:33 +0700

    move cloexec to cis1_proto_utils

--- conanfile.py
+++ conanfile.py
@@ -31,9 +31,9 @@ class Cis1WebUINativeSrvCpp(ConanFile):
                 "OpenSSL/1.1.1a@conan/stable",
                 "sqlite_orm/1.3@tomsksoft/cis1",
                 "cis1_tpl/0.0.1@tomsksoft/cis1",
-                "cis1_cwu_transport/0.0.2@tomsksoft/cis1",
+                "cis1_cwu_transport/0.0.3@tomsksoft/cis1",
                 "cis1_cwu_protocol/0.0.2@tomsksoft/cis1",
-                "cis1_proto_utils/0.0.2@tomsksoft/cis1")
+                "cis1_proto_utils/0.0.3@tomsksoft/cis1")
 
     def build(self):
         cmake = CMake(self)
